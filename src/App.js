8d5f8437a563e9a29a0c89982b1d7e789ecece9e
dimi-fn <dimitris_fanis@protonmail.com>
2022-04-12 18:43:20 +0300

    init react routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
 import React from 'react';
-import {Layout} from './layout';
+import { Routes, Route } from 'react-router-dom';
 
+import { Layout } from './layout';
+import * as Pages from './pages';
+import { Result, Search } from './components';
+
+import './App.css';
+
+const App = () => {
+    return (
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Pages.Home />} />
+        </Route>
+      </Routes>
+    )
+  };
+
+export default App;
