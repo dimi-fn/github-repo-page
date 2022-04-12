fa579735585dcfd31d384e8d5869980a08703a7a
dimi-fn <dimitris_fanis@protonmail.com>
2022-04-13 00:31:36 +0300

    added provider for react redux on index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App';
-
+import { Provider } from 'react-redux';
+import App from './App.js';
+import store from './store';
+import './styles/index.css'
 
 
 ReactDOM.render(
     <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
+        <Provider store = {store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
-  );
+);
