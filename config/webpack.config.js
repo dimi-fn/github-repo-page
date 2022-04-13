b7bf073af0e10743ce1b528d05ecd0985d7bd84c
dimi-fn <dimitris_fanis@protonmail.com>
2022-04-13 23:53:00 +0300

    making favicon work

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -28,7 +28,7 @@ const config = {
     new HtmlWebpackPlugin({
       // used to add the JavaScript code to the HTML
       template: path.join(PUBLIC_DIRECTORY, 'index.html'),
-      favicon: "./src/img/fav.png"
+      favicon: "./src/fav.png"
     }),
   ],
   module: {
