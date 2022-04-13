a8ad4714bfa3abeb38731bab5f7c027c7d53ef19
Henry <uce.henrys@gmail.com>
2022-04-13 11:45:24 +0100

    Updating react rendor for React 18

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 // import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App.js';
 import store from './store';
 import './styles/index.css'
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
+root.render(
     <React.StrictMode>
         <Provider store = {store}>
             <App />
         </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
