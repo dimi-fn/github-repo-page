a533d839ecfc3512af4d3cd1bf6a6c6885a66396
Henry <uce.henrys@gmail.com>
2022-04-13 15:38:06 +0100

    add date filter

--- src/components/RenderResults/index.js
+++ src/components/RenderResults/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 export default function RenderResults({data}) {
+
+    let date = data.created_at.substring(0, data.created_at.indexOf('T'));
+
     return (
         <section role="" className="repoCards">
 
@@ -10,7 +13,7 @@ export default function RenderResults({data}) {
             */}
             <h2><a href={data.html_url} target="_blank">{data.name}</a></h2>
 
-            <p>Created at: {data.created_at}</p>
+            <p>Created at: {date}</p>
             <p>Stars: {data.stargazers_count}</p>
             <p>Number of Forks: {data.forks_count}</p>
             <p>Number of Issues: {data.open_issues_count}</p>
